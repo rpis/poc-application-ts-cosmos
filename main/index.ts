68c1243d15cd420644f3580940b25588f70f7a28
Rafał <rafal@MacBook-Pro-Rafa.local>
2023-01-10 22:44:19 +0100

    corrected return body/error status

--- main/index.ts
+++ main/index.ts
@@ -9,21 +9,27 @@ export async function run(context: Context, req: HttpRequest): Promise<void> {
     applicationService = new ApplicationService();
     await applicationService.init();
   }
+  context.done = () => {};
   if (req.method === 'POST') {
     try {
       applicationService.setExecutionContext(context);
       var id = await applicationService.insert(req.body);
-      context.res.status(200).send({ id: id });
+      context.res = {
+        body: { id: id },
+      };
       return;
     } catch (e) {
       logger.debug(e);
-      context.res.status(500).send(e.message);
+      context.res = {
+        status: 500,
+        body: { e: e.message },
+      };
     }
   }
   if (req.method === 'GET') {
     applicationService.setExecutionContext(context);
     logger.debug(await applicationService.findAll());
-    context.res.status(200).send('Hello!');
+    context.res.status(200).body('Hello!');
     return;
   }
   context.done();
